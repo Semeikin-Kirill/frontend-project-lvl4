6a720eda5c4482322543a4336e508a225d2207b2
Semeikin_Kirill <semeikinkirill21@gmail.com>
2022-06-03 20:32:31 +0300

    feat: added filter words

--- src/components/MessagesBox.jsx
+++ src/components/MessagesBox.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import filter from 'leo-profanity';
 import { useSocket } from '../hooks/index.jsx';
 import { selectorChannels } from '../slices/channelsSlice.js';
 import { selectorMessages } from '../slices/messagesSlice.js';
@@ -11,12 +12,16 @@ function MessagesBox() {
   const currentMessages = messages[currentChannelId]?.map(({ id, author, message }) => (
     <div className="text-break mb-2" key={id}>
       <b>{author}</b>
-      {`: ${message}`}
+      {`: ${filter.clean(message)}`}
     </div>
   ));
 
   const socket = useSocket();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    filter.loadDictionary('ru');
+  }, []);
 
   useEffect(() => {
     socket?.getMessage(dispatch);
